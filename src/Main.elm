010a26b1b302e2f924782f232ef7abf5e21666f4
MainShayne233 <shaynetremblay@hotmail.com>
2018-11-22 00:39:09 -0500

    Basic rendering

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,8 @@
 module Main exposing (Board, Cell(..), Model, Msg(..), Row, init, main, update, view)
 
 import Browser
-import Html exposing (Html, div, h1, img, text)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, img, p, text)
+import Html.Attributes exposing (src, style)
 
 
 
@@ -29,9 +29,9 @@ type alias Model =
 
 initBoard : Board
 initBoard =
-    ( ( Empty, Empty, Empty )
-    , ( Empty, Empty, Empty )
-    , ( Empty, Empty, Empty )
+    ( ( X, X, O )
+    , ( O, X, X )
+    , ( X, O, X )
     )
 
 
@@ -59,19 +59,68 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , renderBoard model.board
+    div containerStyle
+        [ h1 []
+            [ text "Tic Tac Toe" ]
+        , renderBoard
+            model.board
         ]
+
+
+renderCell : Cell -> Html Msg
+renderCell cell =
+    let
+        textContent =
+            case cell of
+                X ->
+                    "X"
+
+                O ->
+                    "O"
+
+                Empty ->
+                    " "
+    in
+    p cellStyle [ text textContent ]
+
+
+renderRow : Row -> Html Msg
+renderRow ( cell1, cell2, cell3 ) =
+    div rowStyle
+        (List.map renderCell [ cell1, cell2, cell3 ])
 
 
 renderBoard : Board -> Html Msg
-renderBoard ( ( topLeft, topCenter, topRight ), ( middleLeft, middleCenter, middleRight ), ( bottomLeft, bottomCenter, bottomRight ) ) =
-    div []
-        [ div []
-            [ text "Top"
-            ]
-        ]
+renderBoard ( topRow, middleRow, bottomRow ) =
+    div boardStyle
+        (List.map
+            renderRow
+            [ topRow, middleRow, bottomRow ]
+        )
+
+
+
+---- STYLE -----
+
+
+containerStyle : List (Html.Attribute Msg)
+containerStyle =
+    [ style "display" "flex", style "justify-content" "center", style "flex-direction" "column", style "align-items" "center" ]
+
+
+cellStyle : List (Html.Attribute Msg)
+cellStyle =
+    [ style "height" "100px", style "width" "100px", style "border" "solid", style "padding" "0", style "margin" "0", style "font-size" "80px" ]
+
+
+rowStyle : List (Html.Attribute Msg)
+rowStyle =
+    [ style "display" "flex" ]
+
+
+boardStyle : List (Html.Attribute Msg)
+boardStyle =
+    [ style "display" "flex", style "flex-direction" "column" ]
 
 
 
